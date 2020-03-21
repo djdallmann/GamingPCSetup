026de468f1b7f708c50d9c575a666a8db4d82c1f
djdallmann <djdallmann@gmail.com>
2020-03-21 10:53:16 -0500

    Create xperfdpcisr.ps1
    Collect Information about DCP and ISR using xperf

---
+++ Scripts/xperfdpcisr.ps1
@@ -0,0 +1,26 @@
+#########
+# Collect Information about DCP and ISR using xperf
+#########
+#Update paths before using
+#Run once to collect the data, run again to stop and generate report.
+
+$fLocation = "D:\PerfMon\xperfstate.txt"
+
+if (Test-Path $fLocation) {
+    $TSTATE = import-clixml -path $fLocation
+} else {
+    $TSTATE = "OFF"
+    $TSTATE | export-clixml -path $fLocation
+}
+
+if ( $TSTATE -eq "ON" ) {
+	$date = date
+	xperf -d "D:\PerfMon\trace.etl"
+	xperf -i D:\PerfMon\trace.etl -o "D:\PerfMon\$($date.ToString("yyyyMMdd-hhmm")).txt" -a dpcisr
+	$TSTATE = "OFF"
+    $TSTATE | export-clixml -path $fLocation
+} else {
+	xperf.exe -on base+interrupt+dpc
+	$TSTATE = "ON"
+    $TSTATE | export-clixml -path $fLocation
+}
